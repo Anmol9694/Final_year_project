6e5da8a84f1e475b27e991c981f323e9f358d924
Anmol9694 <anmolagarwaldance11@gmail.com>
2022-03-13 15:49:11 +0530

    Final year project

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template, request
-import jsonify
-import requests
 import pickle
 import numpy as np
-import sklearn
 from sklearn.preprocessing import StandardScaler
+import xgboost as xgb
+
+
 app = Flask(__name__)
-model = pickle.load(open('random_forest_regression_model.pkl', 'rb'))
+model = xgb.XGBClassifier()
+model.load_model("model.txt")
+
 @app.route('/',methods=['GET'])
 def Home():
     return render_template('index.html')
@@ -17,35 +19,28 @@ standard_to = StandardScaler()
 def predict():
     Fuel_Type_Diesel=0
     if request.method == 'POST':
-        Year = int(request.form['Year'])
-        Present_Price=float(request.form['Present_Price'])
-        Kms_Driven=int(request.form['Kms_Driven'])
-        Kms_Driven2=np.log(Kms_Driven)
-        Owner=int(request.form['Owner'])
-        Fuel_Type_Petrol=request.form['Fuel_Type_Petrol']
-        if(Fuel_Type_Petrol=='Petrol'):
-                Fuel_Type_Petrol=1
-                Fuel_Type_Diesel=0
-        else:
-            Fuel_Type_Petrol=0
-            Fuel_Type_Diesel=1
-        Year=2020-Year
-        Seller_Type_Individual=request.form['Seller_Type_Individual']
-        if(Seller_Type_Individual=='Individual'):
-            Seller_Type_Individual=1
-        else:
-            Seller_Type_Individual=0	
-        Transmission_Mannual=request.form['Transmission_Mannual']
-        if(Transmission_Mannual=='Mannual'):
-            Transmission_Mannual=1
-        else:
-            Transmission_Mannual=0
-        prediction=model.predict([[Present_Price,Kms_Driven2,Owner,Year,Fuel_Type_Diesel,Fuel_Type_Petrol,Seller_Type_Individual,Transmission_Mannual]])
+        age = int(request.form['age'])
+        anaemia = int(request.form['anaemia'])
+        creatinine = float(request.form['creatinine'])
+        diabetes = int(request.form['diabetes'])
+        ejection_fraction = float(request.form['ejection_fraction'])
+        platelets = int(request.form['platelets'])
+        serum_creatinine = float(request.form['serum_creatinine'])
+        serum_sodium = float(request.form['serum_sodium'])
+        sex = int(request.form['sex'])
+        smoking = int(request.form['smoking'])
+        time = float(request.form['time'])
+        death_event = float(request.form['death_event'])
+        temperature = float(request.form['temperature'])
+
+        input_data = [[age, anaemia, creatinine, diabetes, ejection_fraction, platelets, serum_creatinine, serum_sodium, sex, smoking, time, death_event, temperature]]
+
+        prediction=model.predict(np.array(input_data))
         output=round(prediction[0],2)
-        if output<0:
-            return render_template('index.html',prediction_texts="Sorry you cannot sell this car")
+        if output == 0:
+            return render_template('index.html', prediction_text="You don't have High BP")
         else:
-            return render_template('index.html',prediction_text="You Can Sell The Car at {}".format(output))
+            return render_template('index.html', prediction_text="You have high BP")
     else:
         return render_template('index.html')
 
